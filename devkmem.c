c68b5e1837719ecd52b3e11ca2c94ae6c3961697
Jarson Fang <fanghuangcai_idm@163.com>
2015-09-18 10:25:28 +0800

    arguments sanity check

--- devkmem.c
+++ devkmem.c
@@ -16,14 +16,19 @@
 int main(int argc, char* argv[])
 {
     int fd;
-    char *mbase;
-    char read_buf[10];
-    unsigned int regAddr;
-    unsigned int varAddr;
+    void *mbase;
+    unsigned long ptr;
+    unsigned long virAddr;
 
-    varAddr = strtoul(argv[1], 0, 16);
-    
-    unsigned int ptr = varAddr & ~(PAGE_MASK);
+	if(argc < 2) {
+		fprintf(stderr, "\nUsage:\t%s { address }\n"
+			"\taddress : kernel address to act upon\n",
+			argv[0]);
+		exit(1);
+	}
+
+    virAddr = strtoul(argv[1], 0, 16);
+    ptr = virAddr & ~(PAGE_MASK);
 
     fd = open(DEVKMEM, O_RDONLY);
     if (fd == -1) {
@@ -31,13 +36,13 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 
-    mbase = mmap(0,PAGE_SIZE,PROT_READ,MAP_SHARED,fd, (varAddr & PAGE_MASK));
+    mbase = mmap(0, PAGE_SIZE, PROT_READ, MAP_SHARED, fd, (virAddr & PAGE_MASK));
     if (mbase == MAP_FAILED) {
         printf("map failed %s\n",strerror(errno));
     }
     
-    printf("varAddr = 0x%X \n", varAddr);
-    printf("mapbase = 0x%X \n", (unsigned int)mbase);
+    printf("virAddr = 0x%lX \n", virAddr);
+    printf("mapbase = 0x%lX \n", (unsigned long)mbase);
     printf("value   = 0x%X \n",*(unsigned int*)(mbase+ptr));
     printf("char    = %c%c%c%c \n",
     				*(char *)(mbase+ptr), *(char *)(mbase+ptr+1),
